32957c5cfd370564aafdd85dc34870f861c14097
robinzhang <183317744@qq.com>
2012-03-22 17:46:30 +0800

    2

--- Session1/src/com/robin/MainActivity.java
+++ Session1/src/com/robin/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
 	/** Called when the activity is first created. */
@@ -13,14 +14,36 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		findViewById(R.id.button1).setOnClickListener(this);
+		findViewById(R.id.button2).setOnClickListener(this);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+         switch(requestCode){
+         case 1001:
+        	 if(resultCode == RESULT_OK){
+        		 Bundle bundle = data.getExtras();
+        		 String msg = bundle.getString("ReturnVal");
+        		 TextView tv = (TextView)findViewById(R.id.textView1);
+        		 tv.setText(msg);
+        	 }
+        	 break;
+         }
 	}
 
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		Intent intent = null;
 		switch (v.getId()) {
 		case R.id.button1:
-			Intent intent = new Intent(MainActivity.this,SubActivity.class);
+			intent = new Intent(MainActivity.this, SubActivity.class);
+			intent.putExtra("Msg", "from MainActivity!");
 			startActivity(intent);
+			break;
+		case R.id.button2:
+			intent = new Intent(MainActivity.this, SubActivity.class);
+			intent.putExtra("Msg", "from MainActivity!");
+			startActivityForResult(intent, 1001);
 			break;
 		}
 	}
--- Session1/src/com/robin/SubActivity.java
+++ Session1/src/com/robin/SubActivity.java
@@ -1,12 +1,30 @@
 package com.robin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class SubActivity extends Activity {
+public class SubActivity extends Activity implements OnClickListener{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sub);
+		Bundle bundle = getIntent().getExtras(); 
+		if(bundle.containsKey("Msg")){
+			TextView tv = (TextView)findViewById(R.id.textView1);
+			tv.setText(bundle.getString("Msg"));
+		}
+		findViewById(R.id.button1).setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent intent = getIntent();
+		intent.putExtra("ReturnVal", "Return from Sub Activity");
+		setResult(RESULT_OK,intent);
+		finish();
 	}
 }
