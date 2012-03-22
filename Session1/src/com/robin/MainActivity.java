e32a2a94384b066944c89da17f0a99049064bc58
robinzhang <183317744@qq.com>
2012-03-22 11:28:38 +0800

    1

--- Session1/src/com/robin/MainActivity.java
+++ Session1/src/com/robin/MainActivity.java
@@ -1,13 +1,27 @@
 package com.robin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends Activity implements OnClickListener {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		findViewById(R.id.button1).setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.button1:
+			Intent intent = new Intent(MainActivity.this,SubActivity.class);
+			startActivity(intent);
+			break;
+		}
+	}
 }
---
+++ Session1/src/com/robin/SubActivity.java
@@ -0,0 +1,12 @@
+package com.robin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sub);
+	}
+}
